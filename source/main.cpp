845530309071ca33315c0eb5edd3b50571817729
jeruin-dev <jeruinpaulinorosa.dev@gmail.com>
2025-08-31 21:36:32 -0400

    First Window in OpenGl

--- source/main.cpp
+++ source/main.cpp
@@ -1,7 +1,61 @@
-#include <iostream>
+#include <cstddef>
+#include <fmt/base.h>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
 int main(){
-    std::cout << "\n\tHola Mundo\n\n";
 
+    glfwInit();
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT,GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800,600,"Cristal",NULL,NULL);
+
+    if (window == NULL)
+    {
+        fmt::println("Failed to create GLFW window");
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        fmt::println("Failed to initialize GLAD");
+        return -1;
+    }
+
+    glViewport(0,0,800,600);
+
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+
+    while(!glfwWindowShouldClose(window))
+    {
+        processInput(window);
+
+        glClearColor(0.2f,0.3f,0.3f,1.0f);
+
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glfwTerminate();
     return 0;
 }
