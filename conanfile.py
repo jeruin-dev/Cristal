1c28e840fe8bdf8b704cb70b1a1d6015fc150105
jeruin-dev <jeruinpaulinorosa.dev@gmail.com>
2025-08-31 21:11:39 -0400

    Actualizacion de ConanFile.py y agregando glad y glfw

--- conanfile.py
+++ conanfile.py
@@ -1,15 +1,28 @@
 from conan import ConanFile
-from conan.tools.cmake import cmake_layout
+from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout
 
 class Cristal(ConanFile):
     name = "Cristal"
     version = "0.1"
 
     settings = "os", "compiler", "build_type", "arch"
-    generators = "CMakeDeps", "CMakeToolchain"
+    generators = "CMakeDeps"
 
     def requirements(self):
         self.requires("fmt/11.2.0")
+        self.requires("glad/0.1.36")
+        self.requires("glfw/3.4")
 
     def layout(self):
         cmake_layout(self)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.generator = "Ninja"
+        tc.variables["CMAKE_EXPORT_COMPILE_COMMANDS"] = "ON"
+        tc.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
